8175848f9d4e192713b8ba635c81ce2867e829e0
lalehkoupa <lalehkoupa@gmail.com>
2022-07-03 23:53:43 +0100

    create the router dom and each component

--- client/src/components/pages/Homepage.jsx
+++ client/src/components/pages/Homepage.jsx
@@ -1,28 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
-const Homepage = () =>
-{
-	const [data, setData] = useState([]);
+const Homepage = () => {
+  //const [data, setData] = useState([]);
 
-	const getData = async() =>
-	{
-		const res = await fetch("/api/")
-		const data = await res.json()
+  // const getData = async() =>
+  // {
+  // 	const res = await fetch("/api/")
+  // 	const data = await res.json()
 
-		setData(data)
-	}
+  // 	setData(data)
+  // }
 
-	useEffect(() => 
-	{
-		getData()
-	})
+  // useEffect(() =>
+  // {
+  // 	//getData()
+  // })
 
-	return (
-		<div>
-			<h1>This is the Homepage</h1>
-			{data.msg}
-		</div>
-	);
+  return (
+    <div>
+      <h1>This is the Homepage</h1>
+    </div>
+  );
 };
 
 export default Homepage;
