9979832ff6fd38586ce672993e6a817c5176a4cb
Timea <timea.reich@gmail.com>
2022-07-04 15:17:39 +0100

    add jobs/rota.json and create endpoints

--- api/server.js
+++ api/server.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.get("/api", (req, res) => 
-{
-  res.status(200)
-	.json({ success: true, msg: "I'm alive!!!" });
-})
+const jobs = require("./exampleJobs.json");
+const rota = require("./exampleRota.json");
+
+app.get("/api", (req, res) => {
+  res.status(200).json({ success: true, msg: "I'm alive!!!" });
+});
+app.get("/jobs", (req, res) => {
+  res.status(200).json(jobs);
+});
+app.get("/rota", (req, res) => {
+  res.status(200).json(rota);
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
