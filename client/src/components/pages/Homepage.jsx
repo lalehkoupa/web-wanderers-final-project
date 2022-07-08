c0963efc0ad76ad9da7de0ff3001cc48a5cec519
hendo-getagent <hendo@getagent.co.uk>
2022-07-08 13:07:40 +0100

    add homepage back

---
+++ client/src/components/pages/Homepage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Homepage = () => {
+  //const [data, setData] = useState([]);
+
+  // const getData = async() =>
+  // {
+  // 	const res = await fetch("/api/")
+  // 	const data = await res.json()
+
+  // 	setData(data)
+  // }
+
+  // useEffect(() =>
+  // {
+  // 	//getData()
+  // })
+
+  return (
+    <div>
+      <h1>This is the Homepage</h1>
+    </div>
+  );
+};
+
+export default Homepage;
+	
