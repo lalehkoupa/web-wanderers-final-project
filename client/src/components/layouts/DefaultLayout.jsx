7ab7a815423b8b8f79d37525f3ebb339874c874c
hendo-getagent <hendo@getagent.co.uk>
2022-07-09 12:47:39 +0100

    add layout

---
+++ client/src/components/layouts/DefaultLayout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NavBar from "../pages/NavBar";
+import Footer from "../pages/Footer";
+
+const DefaultLayout = ( { children }) =>
+{
+	return (
+		<>
+		<NavBar />
+		{children}
+		<Footer />
+		</>
+	);
+};
+
+export default DefaultLayout;
