9a72162d84fa3fea9e9dfe19df676fa163f85647
Timea <timea.reich@gmail.com>
2022-07-13 01:05:45 +0100

    add hero component

--- client/src/components/molecules/Hero.jsx
+++ client/src/components/molecules/Hero.jsx
@@ -1,8 +1,14 @@
 const Hero = () => {
   return (
-    <div style={{ height: "100px" }} class="text-center bg-primary">
-      <p>Can You Help?</p>
-    </div>
+    <>
+      <div
+        class="hero d-flex align-items-center justify-content-center"
+        style={{ height: "250px" }}
+      >
+        <p class="text-light fs-1">Can You Help?</p>
+      </div>
+      <h2 class="fs-2 text-center my-3">Akwaaba has reopened!</h2>
+    </>
   );
 };
 export default Hero;
