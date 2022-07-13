b6b675410418a2fb187daec2b95ea8196335122c
Timea <timea.reich@gmail.com>
2022-07-13 22:49:03 +0100

    add classes and class row component

--- client/src/components/atoms/Classes.jsx
+++ client/src/components/atoms/Classes.jsx
@@ -2,21 +2,50 @@ import { useEffect, useState } from "react";
 import ClassRow from "../molecules/ClassRow";
 
 const Classes = () => {
-  const [data, setData] = useState();
+  // useEffect(() => {
+  //   fetch("http://localhost:4000/jobs")
+  //     .then((res) => res.json())
+  //     .then((data) => console.log(data));
+  // }, []);
+  const dummyData = [
+    {
+      date: "24-07-2022",
+      availableSpots: 24,
+      filled: 13,
+    },
+    {
+      date: "26-07-2022",
+      availableSpots: 2,
+      filled: 1,
+    },
+    {
+      date: "28-07-2022",
+      availableSpots: 2,
+      filled: 1,
+    },
+    {
+      date: "16-07-2022",
+      availableSpots: 2,
+      filled: 2,
+    },
+  ];
 
-  useEffect(() => {
-    fetch("")
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
+  const [data, setData] = useState(dummyData);
 
-  return data.map((oneClass, index) => (
-    <ClassRow
-      key={index}
-      available={oneClass.availableSpots}
-      date={oneClass.date}
-      filled={oneClass.filled}
-    />
-  ));
+  return (
+    <>
+      <h2 class="text-center fw-bold">Availabe Dates and Slots</h2>
+      {data.map((oneClass, index) => (
+        <ClassRow
+          key={index}
+          available={oneClass.availableSpots}
+          date={oneClass.date}
+          filled={oneClass.filled}
+          startTime={oneClass.startTime}
+          endTime={oneClass.endTime}
+        />
+      ))}
+    </>
+  );
 };
 export default Classes;
--- client/src/components/molecules/ClassRow.jsx
+++ client/src/components/molecules/ClassRow.jsx
@@ -1,13 +1,19 @@
 const ClassRow = ({ available, filled, date }) => {
+  console.log(available);
+  console.log(filled);
+
   return (
     <>
-      <div className="">
-        <p>
+      <div class="container fw-bold d-flex flex-column align-items-center mt-4 bg-yellow_green col-4 pt-2 pb-2">
+        <p class="mb-1 font-weigth-bold">
           {filled} of {available} Slots Filled
         </p>
-        <div className="">
+        <button type="button" class="btn btn-primary mt-0">
+          {date}
+        </button>
+        {/* <div type="button" class="btn btn-primary mt-0">
           <a href="{}">{date}</a>
-        </div>
+        </div> */}
       </div>
     </>
   );
