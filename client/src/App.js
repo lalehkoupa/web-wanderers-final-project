9f399a3735e480627d8800baea2ef8949411fc4f
Timea <timea.reich@gmail.com>
2022-07-15 01:30:00 +0100

    add homepage

--- client/src/App.js
+++ client/src/App.js
@@ -5,22 +5,23 @@ import About from "./components/pages/About";
 import Contact from "./components/pages/Contact";
 import SignUp from "./components/pages/SignUp";
 import Rota from "./components/pages/Rota";
-import DefaultLayout from "./components/layouts/DefaultLayout"
+import DefaultLayout from "./components/layouts/DefaultLayout";
+import Homepage from "./components/pages/Homepage";
 
 const App = () => {
   return (
-		<DefaultLayout>
-			<BrowserRouter>
-				<Routes>
-					{/* I have replace Homepage with HeaderAndNav because there was nothing in Homepage. Also the bootstrap cdn is working as I have tested (check the last about componect for red text class) and removed the unneccessary extra codes from index.html. ByMohammad*/}
-					<Route path="/" element={<Rota />} /> 
-					<Route path="/about" element={<About />} />
-					<Route path="/contact" element={<Contact />} />
-					<Route path="/rota" element={<Rota />} />
-					<Route path="/signup" element={<SignUp />} />
-				</Routes>
-			</BrowserRouter>
-		</DefaultLayout>
+    <DefaultLayout>
+      <BrowserRouter>
+        <Routes>
+          {/* I have replace Homepage with HeaderAndNav because there was nothing in Homepage. Also the bootstrap cdn is working as I have tested (check the last about componect for red text class) and removed the unneccessary extra codes from index.html. ByMohammad*/}
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/rota" element={<Rota />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </DefaultLayout>
   );
 };
 
