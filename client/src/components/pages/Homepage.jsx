3a69e4266c599708937240ee0ad715bc096683be
Timea <timea.reich@gmail.com>
2022-07-16 02:11:37 +0100

    fix typo

--- client/src/components/pages/Homepage.jsx
+++ client/src/components/pages/Homepage.jsx
@@ -8,7 +8,7 @@ const Homepage = () => {
       <Hero />
       <div class="container-fluid text-center bg-yellow_green">
         <p class="pb-2 pt-2 h3 mb-4">
-          New venue: Stoke Newington School, Cclissold Road N16 9EX
+          New venue: Stoke Newington School, Clissold Road N16 9EX
         </p>
       </div>
       <div class="container col-10">
