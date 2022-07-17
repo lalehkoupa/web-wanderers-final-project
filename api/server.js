0a9ce641ade6027270d30281fb3c88636ad7a44a
Alireza Bagheri <alirezabg@gmail.com>
2022-07-17 16:48:23 +0100

    route

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
-const router = require("./api/config/router");
+const router = require("./config/router");
 
 app.use(express.json());
 
