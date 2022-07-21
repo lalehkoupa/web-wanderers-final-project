e6836bf116fe48a955febe52d86c5c12861c26d6
Alireza Bagheri <alirezabg@gmail.com>
2022-07-21 20:31:39 +0100

    Update server.js

--- api/server.js
+++ api/server.js
@@ -3,6 +3,9 @@ const app = express();
 const port = process.env.PORT || 4000;
 const router = require("./config/router");
 const bodyParser = require("body-parser");
+const cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
 
 
