1342a62b1408d759a808e59d448f6642431e6a69
Alireza Bagheri <alirezabg@gmail.com>
2022-07-28 17:37:23 +0100

    auth login and register user
    check if user is registerred and authorised

--- api/controllers/authController.ts
+++ api/controllers/authController.ts
@@ -6,72 +6,85 @@ import { Router } from "express";
 const authRouter = Router();
 
 const SECRET = "alright then, keep your secrets";
+const saltRounds = 10;
+
+authRouter
+  .post("/registerAdmin", async (req, res) => {
+    const { email, password } = req.body;
+    try {
+	  const user = await prisma.user.findUnique({where:{
+		email:email,
+	  },
+	})
+
+	if(!user){
+	  bcrypt.genSalt(saltRounds, function (err, salt) {
+        bcrypt.hash(password, salt, async function (err, hash) {
+          const newUser = await prisma.user.create({
+            data: {
+              email: email,
+              password: hash,
+              firstName: "",
+              lastName: "",
+              phoneNumber: "",
+              userType: 500,
+            },
+          });
+        });
+      });}else{
+		      return res.status(422).json({ message: "user already exist" });
+
+	  }
 
 
-authRouter.post("/registerAdmin", async(req, res) =>{
- 	const { email, password} = req.body;
-	try {
-		//console.log(bcrypt(password));
-		const newUser = await prisma.user.create({
-		data: {
-			email: email,
-			password: password,
-			firstName: "",
-			lastName: "",
-			phoneNumber: "",
-			userType: 500,
-		}
-	});
-		//console.log("new user ", newUser.firstName);
+      return res.status(202).json({ message: `Welcome ${email}!` });
+    } catch (err) {
+      console.log("error", err);
+      return res.status(422).json({ message: err });
+    }
+  })
 
-		return res.status(202).json({ message: `Welcome ${newUser.email}!` });
-	} catch (err) {
-		console.log("error", err);
-		return res.status(422).json({ message: err });
-	}
+  .post("/login", async (req, res) => {
+    const { email, password } = req.body;
 
-})
+    //if(!email) res.status(404).send("No id?");
+    //console.log(email);
+    try {
+      const userToLogin = await prisma.user.findUnique({
+        where: { email: email },
+      });
 
+      
+      if (!userToLogin)
+        return res
+          .status(404)
+          .json({ success: false, msg: "user not register" });
+          const match = await bcrypt.compare(
+            password,
+            userToLogin.password
+          );
+      if (!match) {
+        return res
+          .status(404)
+          .json({ success: false, msg: "password is not correct password" });
+      }
+ if (userToLogin.userType !== 500) {
+   return res.status(404).json({ success: false, msg: "Not authorized" });
+ }
+ 
+      const token = jwt.sign({ sub: userToLogin.id }, SECRET, {
+        expiresIn: "1h",
+      });
 
-.post("/login", async(req, res) =>{
-	const {email,password} = req.body;
-	
-
-	//if(!email) res.status(404).send("No id?");
-	//console.log(email);
-	try {
-		const userToLogin = await prisma.user.findUnique({where: {email: email,},});
-
-		// console.log(userToLogin, password);
-		if(!userToLogin)
-		return res.status(404).json({ success: false, msg: "Woah! Have you registered?"})
-			//throw new ReferenceError("Woah! Have you registered?");
-		if (userToLogin.password!==password){
-			return res.status(404).json({ success: false, msg: "Hmm... That's not correct password"})
-		}
-
-		// const isPasswordCorrect=await bcrypt.compare(password, userToLogin.password);
-		// console.log(isPasswordCorrect);
-		// if( !isPasswordCorrect)
-		// 	//throw new ReferenceError("Hmm... That's not correct");
-		// return res.status(404).json({ success: false, msg: "Hmm... That's not correct password"})
-
-
-		const token = jwt.sign({ sub: userToLogin.id }, SECRET, {
-      expiresIn: "1h",
-    });
-
-		return res
-		.status(200)
-		.json({ message: `Welcome back ${userToLogin.email}`, token });
-
-
-	}catch (error)
-		{
-			console.log("AUTH ERROR!", error);
-			res.status(404).json({ error: true, msg: error });
-		}
-});
-
+      return res
+        .status(200)
+        .json({ message: `Welcome back ${userToLogin.email}`, token });
+    } catch (error) {
+      console.log("AUTH ERROR!", error);
+      res.status(404).json({ error: true, msg: error });
+    }
+  });
 
 export default authRouter;
+
+
