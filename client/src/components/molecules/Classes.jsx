4d5b32d1cd8c48422c493cc12e60e2378125fc87
Timea <timea.reich@gmail.com>
2022-07-28 19:48:43 +0100

    fix homepage

--- client/src/components/molecules/Classes.jsx
+++ client/src/components/molecules/Classes.jsx
@@ -10,44 +10,12 @@ const Classes = () => {
   }, []);
 
   const getData = async () => {
-    const res = await fetch("http://localhost:4000/api/job");
+    const res = await fetch("http://localhost:4000/api/week");
     const data = await res.json();
 
     setData(data);
   };
-  const arrangeJobsByDate = (data) => {
-    let filterArray = [];
-    data.map((item) => {
-      filterArray.push(
-        (({ date, slots, filledSlots }) => ({ date, slots, filledSlots }))(item)
-      );
-    });
-    console.log(filterArray);
-    return filterArray;
-    const sumObject = {};
-  };
-  console.log(arrangeJobsByDate(data));
-  // const filteredArray = [];
 
-  // jobs.map((item) => {
-  //   filteredArray.push((({ date, slots }) => ({ date, slots }))(item));
-  // });
-  // const sumObject = {};
-
-  // filteredArray.map((item) => {
-  //   if (sumObject.hasOwnProperty(item.date)) {
-  //     sumObject[item.date] =
-  //       parseInt(sumObject[item.date]) + parseInt(item.availableSpots);
-  //   } else {
-  //     sumObject[item.date] = item.availableSpots;
-  //   }
-  // });
-  // const sumArray = [];
-
-  // for (let key in sumObject)
-  //   sumArray.push({ date: key, availableSpots: sumObject[key] });
-
-  console.log(data);
   return (
     <>
       <h2 className="text-center fw-bold">Availabe Dates and Slots</h2>
