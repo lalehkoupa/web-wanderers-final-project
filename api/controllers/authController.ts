bf15b6c22a322b6e625dc5b19edeec83c6175781
lalehkoupa <lalehkoupa@gmail.com>
2022-07-28 14:32:25 +0100

    login amin backeend

--- api/controllers/authController.ts
+++ api/controllers/authController.ts
@@ -1,46 +1,53 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import prisma from "../config/prisma";
+import { Router } from "express";
 
-const registerUser = async(req, res) => {
-	const { email, password, firstName, lastName, phoneNumber } = req.body;
+const authRouter = Router();
+
+authRouter.post("/registerAdmin", async(req, res) =>{
+ 	const { email, password} = req.body;
 
 	try {
-		const newUser = await prisma.User.create({
-			data: {
-				email: email,
-				password: password,
-				firstName: firstName,
-				lastName: lastName,
-				phoneNumber: phoneNumber,
-				userType: 500,
-			},
-		});
-
-		console.log("new user ", newUser.firstName);
-		return res.status(202).json({ message: `Welcome ${newUser.username}!` });
+		//console.log(bcrypt(password));
+		const newUser = await prisma.user.create({
+		data: {
+			email: email,
+			password: password,
+			firstName: "",
+			lastName: "",
+			phoneNumber: "",
+			userType: 500,
+		}
+	});
+		//console.log("new user ", newUser.firstName);
+		return res.status(202).json({ message: `Welcome ${newUser.email}!` });
 	} catch (err) {
 		console.log("error", err);
 		return res.status(422).json({ message: err });
 	}
-};
 
-const loginUser = async(req, res) => {
-	const userEmail = req.body.userEmail;
-	const password = req.body.password;
+})
 
-	if(!userEmail) res.status(404).send("No id?");
-	console.log(userEmail);
+.post("/login", async(req, res) =>{
+	const {email,password} = req.body;
+	
+
+	if(!email) res.status(404).send("No id?");
+	//console.log(email);
 	try {
-		const userToLogin = await prisma.user.findUnique({where: {email: userEmail,},});
+		const userToLogin = await prisma.user.findUnique({where: {email: email,},});
 
 		// console.log(userToLogin, password);
 		if(!userToLogin)
-			throw new ReferenceError("Woah! Have you registered?");
+		return res.status(404).json({ success: false, msg: "Woah! Have you registered?"})
+			//throw new ReferenceError("Woah! Have you registered?");
 
-
-		if(!bcrypt.compare(password, userToLogin.password))
-			throw new ReferenceError("Hmm... That's not correct");
+const isPasswordCorrect=await bcrypt.compare(password, userToLogin.password);
+console.log(isPasswordCorrect);
+		if( !isPasswordCorrect)
+			//throw new ReferenceError("Hmm... That's not correct");
+		return res.status(404).json({ success: false, msg: "Hmm... That's not correct password"})
 
 
 		const token = jwt.sign({ sub: userToLogin.id }, "abc", {expiresIn: "7 days",});
@@ -48,10 +55,13 @@ const loginUser = async(req, res) => {
 		return res
 		.status(200)
 		.json({ message: `Welcome back ${userToLogin.email}`, token });
-	} catch (err) {
-		console.log("AUTH ERROR!", err.message);
-		return res.status(422).json({ message: err.message });
-	}
-};
 
-module.exports = { loginUser, registerUser };
+	}catch (error)
+		{
+			console.log("AUTH ERROR!", error);
+			res.status(404).json({ error: true, msg: error });
+		}
+});
+
+
+export default authRouter;
