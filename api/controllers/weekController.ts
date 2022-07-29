66dd9edd5a7b11fbfbd7e8bb4da824e5faf36772
Alireza Bagheri <alirezabg@gmail.com>
2022-07-29 18:55:16 +0100

    update

--- api/controllers/weekController.ts
+++ api/controllers/weekController.ts
@@ -9,140 +9,139 @@ interface Week {
   filledSlots: number;
 }
 
-const checkWeekExists = async(weekDate: string) =>
-{
-	const weekExists = await prisma.week.findMany({
-		where: { weekDate: weekDate },
-		include: { jobs: true }
-	});
+const checkWeekExists = async (weekDate: string) => {
+  const weekExists = await prisma.week.findMany({
+    where: { weekDate: weekDate },
+    include: { jobs: true },
+  });
 
-	if(weekExists)
-		return true;
+  if (weekExists) return true;
 
-	return false;
+  return false;
 };
 
 weeksRouter
-.get("/", async(req, res) => {
-	try {
-		const allWeeks = await prisma.week.findMany({
-      where: {
-        weekDate: { gte: new Date().toLocaleDateString([], {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-  }) },
-      },
-    });
+  .get("/", async (req, res) => {
+    try {
+      const allWeeks = await prisma.week.findMany({
+        orderBy: {
+          weekDate: "asc",
+        },
+      });
 
-		if(!allWeeks) {
-			res
-			.status(404)
-			.json({ error: true, msg: "Cannot find any week " });
-		}
+      if (!allWeeks) {
+        res.status(404).json({ error: true, msg: "Cannot find any week " });
+      }
 
-		return res.status(200).json(allWeeks);
-	} catch (error) {
-		console.log("error ===", error);
-		return res.status(404).json({ error: true, msg: error });
-	}
-})
-.post("/", async(req, res) => {
-	try {
-		const { weekDate, openSlots, filledSlots = 0 } = req.body;
+      return res.status(200).json(allWeeks);
+    } catch (error) {
+      console.log("error ===", error);
+      return res.status(404).json({ error: true, msg: error });
+    }
+  })
+  .post("/", async (req, res) => {
+    try {
+      const { weekDate, openSlots, filledSlots = 0 } = req.body;
 
-		const weekExists = await checkWeekExists(weekDate);
+      const weekExists = await checkWeekExists(weekDate);
 
-		if(weekExists)
-		{
-			return res.status(400).json("This week date already exists");
-			return;
-		}
+      if (weekExists) {
+        return res.status(400).json("This week date already exists");
+        return;
+      }
 
-		const week: Week = await prisma.week.create({
-			data: {
-				weekDate: weekDate,
-				openSlots: openSlots,
-				filledSlots: filledSlots
-			}
-		});
+      const week: Week = await prisma.week.create({
+        data: {
+          weekDate: weekDate,
+          openSlots: openSlots,
+          filledSlots: filledSlots,
+        },
+      });
 
-		return res.status(200).json({ msg: "Week added successfully!", data: week });
+      return res
+        .status(200)
+        .json({ msg: "Week added successfully!", data: week });
+    } catch (error) {
+      console.log("error ===", error);
+      return res.status(404).json({ error: true, msg: error });
+    }
+  })
+  .get("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
 
-	} catch (error) {
-		console.log("error ===", error);
-		return res.status(404).json({ error: true, msg: error });
-	}
-})
-.get("/:id", async(req, res) => {
-	try {
-		const { id } = req.params;
+      const week = await prisma.week.findUnique({
+        where: { id: parseInt(id) },
+        include: { jobs: true },
+      });
 
-		const week = await prisma.week.findUnique({
-			where: { id: parseInt(id) },
-			include: { jobs: true }
-		});
+      if (!week) {
+        return res
+          .status(404)
+          .json({ error: true, msg: "Cannot find this week :(" });
+        return;
+      }
 
-		if(!week) {
-			return res.status(404).json({ error: true, msg: "Cannot find this week :(" });
-			return;
-		}
+      return res.status(200).json(week);
+    } catch (error) {
+      console.log("error ===", error);
+      return res.status(404).json({ error: true, msg: error });
+    }
+  })
+  .delete("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
 
-		return res.status(200).json(week);
-	} catch (error) {
-		console.log("error ===", error);
-		return res.status(404).json({ error: true, msg: error });
-	}
-})
-.delete("/:id", async(req, res) => {
-	try {
-		const { id } = req.params;
+      const weekExists = await prisma.week.findUnique({
+        where: { id: parseInt(id) },
+      });
 
-		const weekExists = await prisma.week.findUnique({ where: { id: parseInt(id) } });
+      if (!weekExists) {
+        res.status(400).json("This week date doesn't exists");
+        return;
+      }
 
-		if(!weekExists)
-		{
-			res.status(400).json("This week date doesn't exists");
-			return;
-		}
+      await prisma.week.delete({ where: { id: parseInt(id) } });
 
-		await prisma.week.delete({ where: { id: parseInt(id) } });
+      return res
+        .status(200)
+        .json({ msg: "Week has been deleted successfully" });
+    } catch (error) {
+      return res.status(404).json({ msg: "we have an error", error: error });
+    }
+  })
+  .patch("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { weekDate, openSlots, filledSlots = 0 } = req.body;
 
-		return res.status(200).json({ msg: "Week has been deleted successfully" });
+      const weekExists = await checkWeekExists(weekDate);
 
-	} catch (error)
-	{
-		return res.status(404).json({ msg: "we have an error", error: error });
-	}
-})
-.patch("/:id", async(req, res) => {
-	try {
-		const { id } = req.params;
-		const { weekDate, openSlots, filledSlots = 0 } = req.body;
+      if (weekExists) {
+        res
+          .status(400)
+          .json(
+            "This week already exists! Please check the params and or week date"
+          );
+        return;
+      }
 
-		const weekExists = await checkWeekExists(weekDate);
+      const week = await prisma.week.update({
+        where: { id: parseInt(id) },
+        data: {
+          weekDate: weekDate,
+          openSlots: openSlots,
+          filledSlots: filledSlots,
+        },
+      });
 
-		if(weekExists)
-		{
-			res.status(400).json("This week already exists! Please check the params and or week date");
-			return;
-		}
-
-		const week = await prisma.week.update({
-			where: { id: parseInt(id) },
-			data: {
-				weekDate: weekDate,
-				openSlots: openSlots,
-				filledSlots: filledSlots
-			}
-		});
-
-		return res.status(200).json({ msg: "Week updated successfully!", data: week });
-
-	} catch (error) {
-		console.log("error ===", error);
-		res.status(404).json({ error: true, msg: error });
-	}
-});
+      return res
+        .status(200)
+        .json({ msg: "Week updated successfully!", data: week });
+    } catch (error) {
+      console.log("error ===", error);
+      res.status(404).json({ error: true, msg: error });
+    }
+  });
 
 export default weeksRouter;
